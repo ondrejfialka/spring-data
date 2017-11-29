b22eebb0bc68645ae8298c3ff12e980973a8bf4b
Ondrej Fialka <ondrej.fialka@unicorncollege.cz>
2017-11-29 11:28:57 +0100

    new project

---
+++ src/cz/ucl/jee/spring/CustomerRepository.java
@@ -0,0 +1,12 @@
+package cz.ucl.jee.spring;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+    List<Customer> findByLastName(String lastName);
+    
+    List<Customer> findByFirstName(String firstName);
+   
+}
